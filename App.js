9821c484ec889394ac39a57c75834f3657f19a15
Kawsar Ahmed <web.kawsarahmed@gmail.com>
2022-10-18 22:34:37 +0600

    added initial setup

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Details from './src/screens/details';
+import Home from './src/screens/home';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Antonio-Medium": require("./assets/fonts/Antonio-Medium.ttf"),
+    "Spartan-Bold": require("./assets/fonts/LeagueSpartan-Bold.ttf"),
+    "Spartan-Regular": require("./assets/fonts/LeagueSpartan-Regular.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Font is loading...</Text>;
+  }
+
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <NavigationContainer theme={DarkTheme}>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <StatusBar style='light' />
+    </>
   );
 }
 
---
+++ src/theme/colors.js
@@ -0,0 +1,11 @@
+
+
+export const colors = {
+    white: '#fff',
+    darkGrey: '#2D0C57',
+    grey: '#9586A8',
+    purple: '#A259FF',
+    lightGreen: '#0BCE83',
+}
+
+
